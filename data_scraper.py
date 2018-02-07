54f419d92ebe93e0fef6c89d3644ab1ac10310c1
phi-line <phi.liney@gmail.com>
2018-02-07 00:55:30 -0800

    basic table data retrieved with beautiful soup

--- data_scraper.py
+++ data_scraper.py
@@ -1,29 +1,50 @@
-from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from os.path import abspath
+
+# 3rd party
 import requests
+from bs4 import BeautifulSoup
 
-headers = {
-    'Origin': 'https://banssb.fhda.edu',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Accept': 'text/html, */*; q=0.01',
-    'Referer': 'https://banssb.fhda.edu/PROD/fhda_opencourses.P_Application?portaldef=201841',
-    'X-Requested-With': 'XMLHttpRequest',
-    'Connection': 'keep-alive',
-}
+SCHEDULE = 'schedule.html'
 
-data = [
-  ('termcode', '201841'),
-]
+def main():
+    # content = mine()
+    content = urlopen(f'file://{abspath(SCHEDULE)}')
+    parse(content)
 
-res = requests.post('https://banssb.fhda.edu/PROD/fhda_opencourses.P_GetCourseList', headers=headers, data=data)
-res.raise_for_status()
+def mine(write=False):
+    headers = {
+        'Origin': 'https://banssb.fhda.edu',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'User-Agent': 'FoothillAPI',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'text/html, */*; q=0.01',
+        'Referer': 'https://banssb.fhda.edu/PROD/fhda_opencourses.P_Application?portaldef=201841',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Connection': 'keep-alive',
+    }
 
-with open('schedule.html', "wb") as file:
-    for chunk in res.iter_content(chunk_size=512):
-        if chunk:
-            file.write(chunk)
+    data = [
+      ('termcode', '201841'),
+    ]
 
-# content = res.content
-# soup = BeautifulSoup(content, 'html5lib')
+    res = requests.post('https://banssb.fhda.edu/PROD/fhda_opencourses.P_GetCourseList', headers=headers, data=data)
+    res.raise_for_status()
+
+    if write:
+        with open(f'{SCHEDULE}', "wb") as file:
+            for chunk in res.iter_content(chunk_size=512):
+                if chunk:
+                    file.write(chunk)
+
+    return res.content
+
+def parse(content):
+    soup = BeautifulSoup(content, 'html5lib')
+    table = soup.find(lambda tag: tag.name == 'table')
+    # rows = table.findAll(lambda tag: tag.name == 'tr')
+    print(rows)
+
+if __name__ == "__main__":
+    main()
