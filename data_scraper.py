7c82ecaaf3b6efb5bbda5912b06cda0191f983be
phi-line <phi.liney@gmail.com>
2018-02-07 21:22:19 -0800

    added TinyDB support. now outputs the class listings as a queryable json

--- data_scraper.py
+++ data_scraper.py
@@ -1,23 +1,33 @@
 from urllib.request import urlopen
-from os.path import abspath
+from os import makedirs
+from os.path import abspath, join, exists
 from collections import namedtuple, defaultdict
-import json
+from json import dumps, JSONEncoder
 import pprint
 
 # 3rd party
 import requests
 from bs4 import BeautifulSoup
+from tinydb import TinyDB, Query
 
 SCHEDULE = 'schedule.html'
 TERM_CODE = '201841'
 HEADERS = ('course', 'CRN', 'desc', 'status', 'days', 'time', 'start', 'end',
            'room', 'campus', 'units', 'instructor', 'seats', 'wait_seats', 'wait_cap')
-
+DB_ROOT = 'db/'
 
 def main():
-    # content = mine(write=True)
+    if not exists(DB_ROOT):
+        makedirs(DB_ROOT, exist_ok=True)
+
+    mine(write=True)
     content = urlopen(f'file://{abspath(SCHEDULE)}')
-    parse(content)
+
+    db = TinyDB(join(DB_ROOT, 'database.json'))
+    parse(content, db=(db or None))
+
+    for table in db:
+        print(table)
 
 
 def mine(write=False):
@@ -49,10 +59,9 @@ def mine(write=False):
     return res.content
 
 
-def parse(content):
-    pp = pprint.PrettyPrinter(depth=4)
+def parse(content, db=None):
+    # pp = pprint.PrettyPrinter(depth=4)
 
-    depts = list()
     soup = BeautifulSoup(content, 'html5lib')
 
     tables = soup.find_all('table', {'class': 'TblCourses'})
@@ -64,22 +73,29 @@ def parse(content):
         for r in rows:
             cols = r.find_all(lambda tag: tag.name == 'td' and not tag.get_text().isspace())
 
-            for i, c in enumerate(cols):
-                a = c.find('a')
-                cols[i] = a.get_text() if a else cols[i].get_text()
+            if cols:
+                for i, c in enumerate(cols):
+                    a = c.find('a')
+                    cols[i] = a.get_text() if a else cols[i].get_text()
 
-            s[f'{cols[2]}'].append(namedtuple('data', HEADERS)(*cols))
+                s[f'{cols[2]}'].append(namedtuple('data', HEADERS)(*cols))
+
         d.sections.append(dict(s))
+        j = {f'{d.dept}': dumps(d, cls=MyEncoder)}
 
-        depts.append(d)
-        pp.pprint(d.__dict__)
-        break
+        db and db.insert(j)
+
+        # pp.pprint(d.__dict__)
 
 class Department():
     def __init__(self, dept, dept_desc):
         self.dept = dept
         self.dept_desc = dept_desc
         self.sections = list()
+
+class MyEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
 
 
 if __name__ == "__main__":
