663be8310514fac0d965f3b089e707c9bb54b412
phi-line <phi.liney@gmail.com>
2018-02-10 14:49:56 -0800

    removed need for tinyTable

--- tinyTable.py
+++
@@ -1,9 +0,0 @@
-from tinydb import TinyDB
-
-class TinyTable():
-    def __init__(self, db):
-        self.db = db
-
-    def get_table(self, name):
-        if name in self.db._table_cache:
-            return self.db._table_cache[name]
