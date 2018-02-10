dd05da468fb154dcb3f761d2e94d6fcf1db4f8be
phi-line <phi.liney@gmail.com>
2018-02-09 18:21:50 -0800

    added query param for dept and section

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from os.path import join, exists
 from json import dumps, load
+from pprint import pprint
 
 # 3rd party
 from quart import Quart, jsonify, request
@@ -10,11 +11,11 @@ app = Quart(__name__)
 
 DB_ROOT = 'db/'
 db = TinyDB(join(DB_ROOT, 'database.json'))
-db = TinyTable(db)
+
 
 @app.route('/')
 async def hello():
-    return 'hello'
+    return 'Foothill API'
 
 
 @app.route('/list', methods=['GET'])
@@ -22,13 +23,26 @@ def api_depts():
     return ', '.join(db.tables()), 200
 
 
-@app.route('/dept/<dept>', methods=['GET'])
-def api_dept(dept):
-    table = db.tables(f'{dept}')
-    if table:
-        return jsonify(table)
-    else:
-        return "", 404
+@app.route('/get', methods=['GET'])
+def api_dept():
+    qp = request.args
+    qp_dept = qp['dept']
+    if qp_dept in db.tables():
+        table = db.table(f'{qp_dept}')
+        entries = table.all()
+
+        if 'section' not in qp:
+            return jsonify(entries), 200
+
+        qp_section = qp['section']
+
+        section = next((e[f'{qp_section}'] for e in entries if f'{qp_section}' in e))
+        if section:
+            print(section)
+            return jsonify(section), 200
+
+
+    return "", 303
 
 
 if __name__ == "__main__":
