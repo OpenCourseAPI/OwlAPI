b84eb4c2f02d40c2368a1dfb2598a00dcd55a858
phi-line <phi.liney@gmail.com>
2018-02-26 23:24:00 -0800

    added basic implementation of batch requests. also added docstrings to all api routes, and made code more modular

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
-from os.path import join, exists
-from json import dumps, load
-from pprint import pprint
+from os.path import join
 
 # 3rd party
 from quart import Quart, jsonify, request
-from tinydb import TinyDB, Query
+from tinydb import TinyDB
 
 app = Quart(__name__)
 
@@ -19,6 +17,14 @@ async def hello():
 
 @app.route('/list', methods=['GET'])
 def api_list():
+    '''
+    `/list` with [GET] handles a single request to get department or course keys from the database
+    It expects a mandatory query parameter `dept` which is first checked for existence and then returns the dept keys.
+    However, if `course` is also selected, it will return only the data of that course within the department.
+    :return: 200 - Found entry and returned keys successfully to the user.
+    :return: 404 - Could not find entry
+    :return:
+    '''
     raw = request.args
     qp = {k: v.upper() for k, v in raw.items()}
 
@@ -34,29 +40,75 @@ def api_list():
     return f"Error! Could not list (this shouldn't happen)", 404
 
 
-@app.route('/get', methods=['GET'])
-def api_dept():
-    raw = request.args
-    qp = {k: v.upper() for k, v in raw.items()}
-
+def get_one(qp: dict()):
+    '''
+    This is a subfunction used by the `/get` route to extract course data.
+    It works for both
+    :return:
+    '''
     qp_dept = qp['dept']
     if qp_dept in db.tables():
         table = db.table(f'{qp_dept}')
         entries = table.all()
 
-        if 'section' not in qp:
-            return jsonify(entries), 200
+        if 'course' not in qp:
+            return entries
 
-        qp_section = qp['section']
+        qp_course = qp['course']
 
         try:
-            section = next((e[f'{qp_section}'] for e in entries if f'{qp_section}' in e))
+            course = next((e[f'{qp_course}'] for e in entries if f'{qp_course}' in e))
         except StopIteration:
-            return f'Error! Could not find section: {qp_section}', 404
-        if section:
-            return jsonify(section), 200
+            return dict()
+        if course:
+            return course
 
-    return f'Error! Could not find department: {qp_dept}', 404
+
+@app.route('/get', methods=['GET'])
+def api_one():
+    '''
+    `/get` with [GET] handles a single request to get a whole department or a whole course listing from the database
+    It expects a mandatory query parameter `dept` and an optionally `course`. ex.{'dept': 'CS', 'course': '2C'}
+    If only `dept` is requested, it checked for its existence in the database and then returns it.
+    However, if `course` is also selected, it will return only the data of that course within the department.
+    :return: 200 - Found entry and returned data successfully to the user.
+    :return: 404 - Could not find entry
+    '''
+    raw = request.args
+    qp = {k: v.upper() for k, v in raw.items()}
+
+    data = get_one(qp)
+    json = jsonify(data)
+    return (json, 200) if data else ('Could not find given parameters in database', 404)
+
+
+@app.route('/get', methods=['POST'])
+async def api_many():
+    '''
+    `/get` with [POST] handles a batch request to get many departments or a many course listings from the database.
+    This batch request is meant to simulate hitting the api route with this data N times.
+    It expects a mandatory list of objects containing keys `dept` and `course`.
+
+    Example:
+        {
+            "courses": [
+                {"dept": "MATH", "course": "1A"},
+                {"dept": "ENGL", "course": "1A"},
+                {"dept": "CHEM", "course": "1A"}
+            ]
+        }
+    :return: 200 - Found all entries and returned data successfully to the user.
+    :return: 404 - Could not find one or more entries.
+    '''
+    raw = await request.get_json()
+    data = []
+
+    for qp in raw['courses']:
+        d = get_one(qp)
+        data.append(d)
+
+    json = jsonify({'courses': data})
+    return (json, 200) if data else ('Could not find given parameters in database', 404)
 
 
 if __name__ == "__main__":
