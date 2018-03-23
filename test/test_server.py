7fec0c92f53fb1d210317b704847311a9e27a706
tryexceptelse <schwarzjohnv@gmail.com>
2018-03-22 22:03:06 -0700

    added sample test to test_server module

---
+++ test/test_server.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from server import generate_url
+
+
+class TestGenerateURL(TestCase):
+    def test_sample_url_can_be_generated(self):
+        self.assertEqual(
+            'get?dept=test_dept&course=test_course',
+            generate_url('test_dept', 'test_course')
+        )
