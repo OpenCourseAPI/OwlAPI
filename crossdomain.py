5b71f6c8d85531dbec395e0b35039eb466d1a075
phi line <phi.liney@gmail.com>
2018-04-09 10:16:07 +0000

    removed [OPTIONS] from CORS functionality

--- crossdomain.py
+++ crossdomain.py
@@ -1,8 +1,3 @@
 def add_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
-    if request.method == 'OPTIONS':
-        response.headers['Access-Control-Allow-Methods'] = 'GET, POST'
-        headers = request.headers.get('Access-Control-Request-Headers')
-        if headers:
-            response.headers['Access-Control-Allow-Headers'] = headers
     return response
