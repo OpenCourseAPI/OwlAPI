aa3ce740f0efce461f2ac1faeb0860e057fc3741
tryexceptelse <schwarzjohnv@gmail.com>
2018-06-10 09:48:21 -0700

    Condensed generate_test_data; slightly more readable now?

--- tests/generate_test_data.py
+++ tests/generate_test_data.py
@@ -17,33 +17,25 @@ def generate_data_py():
     with open(os.path.join(TEST_DATA_DIR, 'data.py'), 'w') as file:
         # test_sample_url_can_be_generated
         data = {'dept': 'test_dept', 'course': 'test_course'}
-
         file.write(f"test_sample_url_can_be_generated_data = {data}\n")
 
         # test_get_one_dept
         data = {'dept': 'CS'}
-
         dept = test_database.table(f"{data['dept']}").all()
-
         file.write(f"test_get_one_dept_data = {dept}\n")
 
         # test_get_one_dept_and_course
         data = {'dept': 'CS', 'course': '2A'}
-
         dept = test_database.table(f"{data['dept']}").all()
-
         course = next((e[f"{data['course']}"] for e in dept if
                        f"{data['course']}" in e))
-
         file.write(f"test_get_one_dept_and_course_data = {course}\n")
 
         # test_get_two_dept
         data = {'courses': [{'dept': 'CS'}, {'dept': 'MATH'}]}
-
         depts = []
         for i in data['courses']:
             depts.append(test_database.table(i['dept']).all())
-
         file.write(f"test_get_two_dept_data = {depts}\n")
 
 
