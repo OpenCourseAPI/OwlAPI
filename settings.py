21a397bc0716dac6fdc5e3829ce6ad3cf894987a
phi-line <phi.liney@gmail.com>
2018-06-10 18:53:52 -0700

    fixed edgecase for past quarters

--- settings.py
+++ settings.py
@@ -74,5 +74,37 @@ ADVANCED_FORM_DATA = [
          ('end_ap', 'a'),
          ('SUB_BTN', 'Section Search'),
          ('path', '1')]
+    ],
+
+    [
+        [('sel_subj', 'dummy'),
+         ('sel_day', 'dummy'),
+         ('sel_schd', 'dummy'),
+         ('sel_insm', 'dummy'),
+         ('sel_camp', 'dummy'),
+         ('sel_levl', 'dummy'),
+         ('sel_sess', 'dummy'),
+         ('sel_instr', 'dummy'),
+         ('sel_ptrm', 'dummy'),
+         ('sel_attr', 'dummy')],
+        [('sel_crse', ''),
+         ('sel_title', ''),
+         ('sel_schd', '%'),
+         ('sel_from_cred', ''),
+         ('sel_to_cred', ''),
+         ('sel_camp', '%'),
+         ('sel_levl', '%'),
+         ('sel_ptrm', '%'),
+         ('sel_instr', '%'),
+         ('sel_sess', '%'),
+         ('sel_attr', '%'),
+         ('begin_hh', '0'),
+         ('begin_mi', '0'),
+         ('begin_ap', 'a'),
+         ('end_hh', '0'),
+         ('end_mi', '0'),
+         ('end_ap', 'a'),
+         ('SUB_BTN', 'Section Search'),
+         ('path', '1')]
     ]
 ]
